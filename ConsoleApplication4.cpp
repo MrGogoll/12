00d920e1d19dd9f888b3f64d8bdbb809d830e766
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-06 15:27:25 +0300

    12

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+	int  a,b;
+	b = 0;
+	cin >> n;
+	for  (int i = 0;  i < n; i++)
+	{
+		cin >> a;
+		b += a;
+	}
+	cout << b / n;
+}
+
